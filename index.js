504c28f1e16fef70d8673675703d76ffcc0d37f8
karen diaz <106630634+karenhoran@users.noreply.github.com>
2022-11-01 23:19:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hola Mundo")
+})
+
+app.listen(port, () => {
+    console.log("Mi puerto es " + port)
+})
+
